f03cb7ba3e805ae9410387ec75637f9cab8edd98
Kyle Weiland <kyle.weiland@improving.com>
2023-10-19 13:31:09 -0400

    Init commit

---
+++ CocktailRecipeLookup.Api/Controllers/DrinksController.cs
@@ -0,0 +1,37 @@
+﻿using CocktailRecipeLookup.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CocktailRecipeLookup.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DrinksController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public DrinksController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> SearchDrinks(string query)
+        {
+            var apiUrl = $"YOUR_NINJAS_COCKTAIL_API_ENDPOINT_HERE?search={query}";
+            var response = await _httpClient.GetAsync(apiUrl);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest("Error fetching drinks.");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var drinks = JsonConvert.DeserializeObject<List<Drink>>(content); // Adjust based on actual API response structure
+
+            return Ok(drinks);
+        }
+    }
+}
+
---
+++ CocktailRecipeLookup.Api/Models/Drink.cs
@@ -0,0 +1,10 @@
+﻿namespace CocktailRecipeLookup.Api.Models
+{
+    public class Drink
+    {
+        public string? Name { get; set; }
+        public List<string>? Ingredients { get; set; }
+        public string? Instructions { get; set; }
+        
+    }
+}
