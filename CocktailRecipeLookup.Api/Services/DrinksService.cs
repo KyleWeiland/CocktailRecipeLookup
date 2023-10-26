ab5441eef30c02f646b5b5dd2d3e287491ea5916
Kyle Weiland <kyle.weiland@gmail.com>
2023-10-26 01:20:44 -0400

    Created/Implemented methods to Drinks Service to convert cl to ml in drinks' ingredients.

--- CocktailRecipeLookup.Api/Services/DrinksService.cs
+++ CocktailRecipeLookup.Api/Services/DrinksService.cs
@@ -1,5 +1,6 @@
 using CocktailRecipeLookup.Api.Models;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace CocktailRecipeLookup.Api.Services
 {
@@ -19,6 +20,7 @@ namespace CocktailRecipeLookup.Api.Services
             _httpClient.DefaultRequestHeaders.Add("X-Api-Key", _apiKey);
             var response = await _httpClient.GetStringAsync($"https://api.api-ninjas.com/v1/cocktail?name={name}");
             var drinks = JsonConvert.DeserializeObject<List<Drink>>(response);
+            ConvertInstructions(drinks);
             return drinks;
         }
 
@@ -34,6 +36,37 @@ namespace CocktailRecipeLookup.Api.Services
             var response = await _httpClient.GetStringAsync($"https://api.api-ninjas.com/v1/cocktail?ingredients={combined}");
             var drinks = JsonConvert.DeserializeObject<List<Drink>>(response);
             return drinks;
+        }
+
+        private void ConvertInstructions(List<Drink> drinks)
+        {
+            // Skip if none of the ingredients have cl
+            if (!drinks.Any(d => d.Ingredients.Any(i => i.Contains(" cl ")))) return;
+
+            foreach (var d in drinks)
+            {
+                for (int j = 0; j < d.Ingredients.Count; j++)
+                {
+                    d.Ingredients[j] = ConvertClToMl(d.Ingredients[j]);
+                }
+            }
+        }
+
+        private string ConvertClToMl(string ingredient)
+        {
+            // Use regex to match the pattern of a number followed by "cl"
+            var regex = new Regex(@"(\d+(\.\d+)?)\s*cl");
+
+            // Use MatchEvaluator to convert the matched value to ml
+            return regex.Replace(ingredient, m =>
+            {
+                // Parse the number and multiply by 10 to convert to ml
+                double valueInCl = double.Parse(m.Groups[1].Value);
+                double valueInMl = valueInCl * 10;
+
+                // Return the converted value with "ml"
+                return $"{valueInMl} ml";
+            });
         }
     }
 }
