fa422e9034cfc8378772e257febae0211ead3251
Kyle Weiland <kyle.weiland@gmail.com>
2023-10-31 00:31:54 -0400

    Removed unused header.

--- cocktail-reciple-lookup-ui/src/App.js
+++ cocktail-reciple-lookup-ui/src/App.js
@@ -3,7 +3,7 @@ import CocktailSearch from "./CocktailSearch";
 function App() {
   return (
     <div>
-      <h1>Welcome to Cocktail Finder v3</h1>
+      <h1>Welcome to Cocktail Finder v4</h1>
       <CocktailSearch></CocktailSearch>
       <h6>Now with pipelines :)</h6>
     </div>
