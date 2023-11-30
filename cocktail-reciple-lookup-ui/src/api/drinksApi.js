d3245b24d698f81baec7931b0ed894f3619840fe
Kyle Weiland <kyle.weiland@improving.com>
2023-11-29 21:32:40 -0500

    cont.

--- cocktail-reciple-lookup-ui/src/api/drinksApi.js
+++ cocktail-reciple-lookup-ui/src/api/drinksApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const drinksApi = axios.create({
-    baseURL: 'https://localhost:7033/api/', 
-    //baseUrl: 'https://api.kyle-weiland.com/api/',
+    //baseURL: 'https://localhost:7033/api/', 
+    baseUrl: 'https://api.kyle-weiland.com/api/',
     headers: {
         'Content-Type': 'application/json',
     }
